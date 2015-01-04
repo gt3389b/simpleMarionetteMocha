1a36acab064fb7ef59bfb4919108c4732786e732
Russell Leake <gt3389b@gmail.com>
2015-01-04 07:01:21 -0500

    Add application personlization

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -10,6 +10,31 @@
  */
 
 module.exports.bootstrap = function(cb) {
+	// This will extend our app.locals with those provided in config.http
+	_.extend(sails.hooks.http.app.locals, sails.config.http.locals);
+
+	locals = sails.hooks.http.app.locals;
+
+	// TODO:  Would like to read these from a configuration file or environment settings
+	var configObj = {
+		company_name: "Kabaddi International, Inc.",
+		company_description: "The A-team, the top 10%",
+		auth_method: "local"  // local authentication
+	}
+
+	// TODO:  Would also like to update (if specified) the configuration from 
+	//  a file (i.e. if the database already has data)
+	App.find(function(err, data) {
+		if(err) console.log("ERROR accessing app_config");
+
+		if(data && data.length) {
+			locals.app_config = _.clone(data);
+		} else if (data && !data.length) {
+			App.create(configObj, function userCreated(err, configObj) {
+				locals.app_config = _.clone(configObj);
+			});		
+		}
+	});
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
